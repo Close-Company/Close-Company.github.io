125fdd7fba96aaa07339cd3d36013a4bceeb02d6
renanpk <renan_g_miranda@hotmail.com>
2025-02-14 07:11:39 -0300

    fix/logo

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ import 'firebase_options.dart';
 FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
